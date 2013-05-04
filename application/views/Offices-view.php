a45ff4e6d4731f906f5d8fbaa9f142c20cfe4bb9
Ivan Bohun <drivanbohun@gmail.com>
2013-05-04 14:39:56 -0500

    offices

--- application/views/Offices-view.php
+++ application/views/Offices-view.php
@@ -1,21 +1,33 @@
 	<!-- //LOCATION: /q/Attorney-Northbrook-Skokie-Offices/Legal-Consultation-Places.html 
 -->
 <div id="content">
-	<div class="innerContent"><h1>Offices</h1>
-	
-	<ul class="vcard">
-	  
-	  <li class="street-address">4023 Church St. </li>
-	  <li class="locality">Skokie</li>
-	  <li><span class="state">IL</span>, <span class="zip">60076</span></li>
-	  <li class="email"><a href="#">g.baltar@cmail.com</a></li>
-	</ul>
-</div>
+	<div class="innerContent">
+		<h1>Offices</h1>
+		
+		<div class="officesWrapper">
+			<ul class="vcard">
+				
+				<li class="street-address">4023 Church St. </li>
+				<li class="locality">Skokie</li>
+				<li><span class="state">IL</span>, <span class="zip">60076</span></li>
+				<li class="email"><a href="#">g.baltar@cmail.com</a></li>
+			</ul>
+			
+			<ul class="vcard">
+				
+				<li class="street-address">1438 Techny St. </li>
+				<li class="locality">Northbrook</li>
+				<li><span class="state">IL</span>, <span class="zip">60062</span></li>
+				<li class="email"><a href="#">g.baltar@cmail.com</a></li>
+			</ul>
+		</div>
+
+	</div>
 
 </div>
 <!-- //LOCATION: home/ -->
 <script type="text/javascript">
 
-	menuModifier(4);
+menuModifier(4);
 
 </script>
