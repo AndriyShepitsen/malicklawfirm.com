a142856f6a27a900207497a44c06f11a1efddbc7
Ivan Bohun <drivanbohun@gmail.com>
2013-05-04 17:26:53 -0500

    news

--- application/views/News-view.php
+++ application/views/News-view.php
@@ -2,37 +2,43 @@
 	<div class="innerContent">
 		<h1>Legal Corner News</h1>
 		<hr/>
-	<h4 class="radius secondary label">Federal Estate Tax Exemption</h4>
 
-	<p>The current Federal Estate Tax exemption (reflected as a credit equivalent) has increased to 
-		$2.0 million dollars, for decedents dying after January 1, 2006. This may affect the allocation 
-		between marital (Trust A) and by-pass trusts (Trust B or Credit shelter), depending upon the 
-		Trust language used in the underlying trust document. This may also affect the amount qualifying 
-		for the marital deduction and available for the surviving spouse. The exemption will increase to
-		$3.5 million for decedents dying after January 1, 2009. Check back for more updates as Congress 
-		debates this important estate planning regulation.
-	</p>
-	<hr/>
-
-	<h4 class="radius secondary label">Health Insurance Portability and Accountability Act of 1996 </h4>
-
-	<p>A new law was enacted recently called the Health Insurance Portability and Accountability Act of 
-		1996 (aka HIPAA), 42 USC 1320d and 45 CFR 160-164. A provision, which we attach at the end of the 
-		Power of Attorney for Health Care, enables your Agent to access all of your medical records after 
-		you have become incapacitated. This is a necessity if you expect your Agent to act in your best 
-		interest after you have been deemed incapable of making your own decisions. 
-		How can the Agent make the best decision if they don’t have access to your medical history? 
-		I strongly encourage you to contact our law firm regarding this new law, if you have not already 
-		done so, and either add it to your existing Power of Attorney, or create a new, 
-		updated Power of Attorney.
-	</p>
-<hr/>
-	<h4 class="radius secondary label">State of Illinois Probate Court</h4>
+		<h4 class="radius secondary label">State of Illinois Probate Court</h4>
 
 	<p>The State of Illinois has increased the minimum amount of property, held in your name alone subject to the Probate jurisdiction, from $50,000 to $100,000. This means that upon your death, all assets held in your name alone that total over $100,000 must be subjected to the Probate court process. This process wastes time, money and energy for the Nominated Executor of your Estate. Save your loved ones a nuisance by creating a Living Trust, and by transferring your assets into that Trust. Assets in your name alone, under $100,000, may now be transferred by Small Estate Affidavit, prepared 30 days after the death of the decedent. Any estates that are transferred by affidavit are not subject to jurisdiction by the Probate Court. The estate totaling less than $100,000, however, will still be subject to creditors’ claims for two (2) years after transfer by affidavit. </p>
 	<p>Some assets you own are outside of the Probate Court’s jurisdiction, but you still want to make sure the beneficiaries of these assets are designated correctly. These assets include: 401K’s or other retirement plans, IRA plans, Life insurance, and some savings accounts or CDs. In order to make the transition smoother after your death, make sure the beneficiaries for these assets are 1) correctly designated the way you would like them, and 2) clearly designated on all documents, to avoid confusion and resentment from your descendants. In addition to clarifying the beneficiary designation, you may want to investigate the tax consequences of beneficiaries withdrawing the principal of these plans after your death. One option many companies offer is to enable the spouse to roll-over your 401K into his or her own IRA. This will prevent the spouse from being taxed on the full amount on their individual income tax for the year of withdrawal.</p>
 	
 
+	<h4 class="radius secondary label">Federal Estate Tax Exemption 2013</h4>
+
+	<p>The Estate Tax is a tax on your right to transfer property at your death. 
+		It consists of an accounting of everything you own or have certain interests 
+		in at the date of death (Refer to Form 706 (PDF)). The fair market value of these 
+		items is used, not necessarily what you paid for them or what their values were
+		 when you acquired them. The total of all of these items is your "Gross Estate." 
+		 The includible property may consist of cash and securities, real estate, insurance, 
+		 trusts, annuities, business interests and other assets. <p/>
+
+<p/>Once you have accounted for the Gross Estate, certain deductions (and in special circumstances, 
+reductions to value) are allowed in arriving at your "Taxable Estate." 
+These deductions may include mortgages and other debts, estate administration expenses, property that passes to surviving spouses and qualified charities. The value of some operating business interests or farms may be reduced for estates that qualify.
+
+After the net amount is computed, the value of lifetime taxable gifts 
+(beginning with gifts made in 1977) is added to this number and the tax is computed. 
+The tax is then reduced by the available unified credit.<p/>
+
+<p/>Most relatively simple estates (cash, publicly traded securities, 
+small amounts of other easily valued assets, and no special deductions or elections, 
+or jointly held property) do not require the filing of an estate tax return. 
+A filing is required for estates with combined gross assets and prior taxable 
+gifts exceeding $1,500,000 in 2004 - 2005; $2,000,000 in 2006 - 2008; $3,500,000 
+for decedents dying in 2009; and $5,000,000 or more for decedent's dying in 2010 and 
+2011 (note: there are special rules for decedents dying in 2010); $5,120,000 in 2012 
+and $5,250,000 in 2013.
+	</p>
+
+
+	
 	</div>
 </div>
 <!-- //LOCATION: q/home/ -->
