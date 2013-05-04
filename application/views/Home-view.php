b4e7a95dd3af1ccb1029ab39ed0bd50af9c329df
Ivan Bohun <drivanbohun@gmail.com>
2013-05-04 11:26:20 -0500

    Portrait Container

--- application/views/Home-view.php
+++ application/views/Home-view.php
@@ -7,37 +7,46 @@
 			Our firm placed high priority on personal service, friendliness and responsiveness to cherish a strong long-lasting relationship.	We work with clients to create innovative solutions and execute legal strategies in the most effective way. At some point or another, we all need a legal professional to advice us and we hope you will let our firm partner up with you. 
 		</p>
 
-			<div class="practiceAreas">Practice Areas:</div>
+		<div class="practiceAreas">Practice Areas:</div>
 
-			<ul class="disc" >
-				<li>Commercial and Residentional Real Estate Transactions</li>
-				<li>Estate planning - Wills and Trusts</li>
-				<li>Probate</li>
-				<li>Trust Administration</li>
-				<li>Business and Corporate Law</li>
-				<li>Family Law (uncontested divorces)</li>
+		<ul class="disc" >
+			<li>Commercial and Residentional Real Estate Transactions</li>
+			<li>Estate planning - Wills and Trusts</li>
+			<li>Probate</li>
+			<li>Trust Administration</li>
+			<li>Business and Corporate Law</li>
+			<li>Family Law (uncontested divorces)</li>
+		</ul>
+
+	</div>
+
+	<div id="portraitContainer"> 
+		<img id="portraitAnitaMalick" src="<?php echo base_url();?>/comp/img/Black_Anita_portret_200px.jpg" alt="Northbrook Skokie principal attorney president">
+
+		<div id="anitaMalickPortraitText">Anita Malick</div>
+		<div id="principalAttorney"> Principal Attorney</div>
+		<div id="presidentPortraitText">President <br>
+
+			of The Malick Law Firm</div>
+
+			<div id="located">LOCATED AT: </div>
+
+			<ul class="vcard">
+
+				<li class="street-address">1438 Techny Road,</li>
+				<li class="locality">Northbrook</li>
+				<li><span class="state">IL</span>, <span class="zip">60062</span></li>
+				<li class="email"><a href="#">info@malicklawfirm.com</a></li>
 			</ul>
 
-		</div>
-
-		<div id="portraitContainer"> 
-			<img id="portraitAnitaMalick" src="<?php echo base_url();?>/comp/img/Black_Anita_portret_200px.jpg" alt="Northbrook Skokie principal attorney president">
-
-			<div id="anitaMalickPortraitText">Anita Malick</div>
-			<div id="principalAttorney"> Principal Attorney</div>
-			<div id="presidentPortraitText">President <br>
-
-				of The Malick Law Firm</div>
-
-				<div id="located">LOCATED AT: </div>
-				<div id="technyRoad">1438 Techny Road,<br>
+				<!-- <div id="technyRoad">1438 Techny Road,<br>
 					Northbrook, IL
 				
 					<div id="phoneNumber">(224) 235-4924</div>
-
+				
 					<div id="emailAddress">info@malicklawfirm.com </div>
-
-				</div>
+				
+				</div> -->
 
 			</div>
 <!-- //LOCATION: q/home/
@@ -46,7 +55,7 @@
 
 $(function() {
 	
-menuModifier(1);
+	menuModifier(1);
 });
 
 </script>	
