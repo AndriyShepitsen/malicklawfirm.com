8cf9d94abd292f3244eaea89c08bb95db1494ca9
IvanBohun <drivanbohun@gmail.com>
2013-05-07 13:40:55 -0500

    offices

--- application/views/Offices-view.php
+++ application/views/Offices-view.php
@@ -3,7 +3,7 @@
 <div id="content">
 
 	<div class="innerContentLocations"><h1>Our Locations</h1> <hr/>
-	<div id="skokieMap">
+<div class='LocationWrapper'>	<div id="skokieMap">
 <iframe src="http://www.map-generator.org/7a724f8c-c4f1-4cbc-a4fe-85be64872ae9/iframe-map.aspx" scrolling="no" height="350px" width="700px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
 	</div>
 
@@ -12,12 +12,23 @@
     4023 Church St.<br/>
     Skokie, IL, 60076 <br/>
     (847) 674-9797<br/>
+</div></div>
+
+<div class='LocationWrapper'>	<div id="NorthbrookMap">
+<iframe src="http://www.map-generator.org/35e36f07-5398-4206-9517-ba68863623a9/iframe-map.aspx" scrolling="no" height="350px" width="700px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+	</div>
+
+<div id='OfficeDivNorthBrook'>
+	<div class='OfficeP'>NorthBrook Office</div>
+    1438 Techny St.<br/>
+    Northbrook IL, 60062<br/>
+    (224) 235-4924<br/>
+</div></div>
+
+</div>
 </div>
 
-
-
-</div>
-</div>
+    
 
 
 <script type="text/javascript">
